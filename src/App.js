2487817d757b6226941112bc90b7664359359c09
halosatrio <loredana@disroot.org>
2021-02-09 14:07:54 +0700

    create main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexLastPost = currentPage * postPerPage;
+  const indexFirstPost = indexLastPost - postPerPage;
+  const currentPosts = posts.slice(indexFirstPost, indexLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">Blog Posts</h1>
+    </div>
+  );
 }
 
 export default App;
